76dd6f07165f0fec124d449095b152bb4868d9af
doreennjagi <jaydenrishi@gmail.com>
2025-06-03 14:38:01 +0300

    added sql queries

--- advanced_queries.sql
+++ advanced_queries.sql
@@ -8,3 +8,18 @@ JOIN products p ON od.product_id = p.product_id
 GROUP BY customer_name
 ORDER BY total_spent DESC
 LIMIT 3;
+SELECT 
+  DATE_FORMAT(o.order_date, '%Y-%m') AS month,
+  SUM(od.quantity * p.price) AS revenue
+FROM orders o
+JOIN order_details od ON o.order_id = od.order_id
+JOIN products p ON od.product_id = p.product_id
+GROUP BY month
+ORDER BY month;
+SELECT 
+  o.order_id,
+  COUNT(od.product_id) AS product_count
+FROM orders o
+JOIN order_details od ON o.order_id = od.order_id
+GROUP BY o.order_id
+HAVING product_count > 2;
