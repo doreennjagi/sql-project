4bff54429d44dd019a802012f1663800acf5dd93
doreennjagi <jaydenrishi@gmail.com>
2025-06-03 14:27:43 +0300

    added sql

---
+++ advanced_queries.sql
@@ -0,0 +1,18 @@
+SELECT 
+CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
+  SUM(od.quantity * p.price) AS total_spent
+FROM customers c
+JOIN orders o ON c.customer_id = o.customer_id
+JOIN order_details od ON o.order_id = od.order_id
+JOIN products p ON od.product_id = p.product_id
+GROUP BY customer_name
+ORDER BY total_spent DESC
+LIMIT 3;
+SELECT 
+DATE_FORMAT(o.order_date, '%Y-%m') AS month,
+  SUM(od.quantity * p.price) AS revenue
+FROM orders o
+JOIN order_details od ON o.order_id = od.order_id
+JOIN products p ON od.product_id = p.product_id
+GROUP BY month
+ORDER BY month;
